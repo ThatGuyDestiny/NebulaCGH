c1412182a62cf00bb1a4d2e8560755f025968159
Ayce B (GreenWorld) <81369743+GreenyDEV@users.noreply.github.com>
2022-01-14 19:35:19 +0000

    imp

--- public/dil.js
+++ public/dil.js
@@ -1,84 +1,57 @@
 // Open Optimized* Optimized by https://jscompress.com/ 
 $(".menu").click(function(){$(this).toggleClass("icon-menu"),$(this).toggleClass("icon-cross"),$("nav").toggleClass("down"),$("nav li a").removeClass("down"),$(".search").removeClass("down"),$(".icon-search").removeClass("icon-cross")}),$("nav li a").click(function(){$(".menu").addClass("icon-menu"),$(".menu").removeClass("icon-cross"),$("nav").toggleClass("down")}),$(".icon-search").click(function(){$(this).toggleClass("icon-cross"),$(".menu").addClass("icon-menu"),$(".menu").removeClass("icon-cross"),$(".search").toggleClass("down"),$("nav").removeClass("down")});
-// Close optimized 
+function tab(){var t=prompt("Enter a title");""!==t?(localStorage.setItem("title",t),document.title=t):(localStorage.removeItem("title"),document.title="‎")}window.addEventListener("load",function(){var t=localStorage.getItem("title");null!==localStorage.getItem("title")?document.title=t:document.title="NEBULA"});
+var modal=document.getElementById("myModal"),btn=document.getElementById("myBtn"),span=document.getElementsByClassName("close")[0];function discordlol(){window.location.replace("https://justcool.thejangoteam.repl.co/service/hvtrs8%2F-dksaopd%2Ccmm-arp/")}function history(){var t=prompt("Please insert a URL: https://example.com"),e=prompt("Please insert another URL: https://example.com"),o=prompt("Please insert one last URL: https://example.com");t.value,e.value,o.value;window.open(t,"_blank"),window.open(e,"_blank"),window.open(o,"_blank")}function popup(t,e){if(!window.focus)return!0;t="string"==typeof t?t:t.href;return window.open(t,e,"width=400,height=200,scrollbars=yes"),!1}btn.onclick=function(){modal.style.display="block"},span.onclick=function(){modal.style.display="none"},window.onclick=function(t){t.target==modal&&(modal.style.display="none")};
 
-// TAB CLOAK
-function tab() {
-  let title = prompt("Enter a title")
-  if (title !== "") {
-    localStorage.setItem("title", title)
-    document.title = title
-  } else {
-    localStorage.removeItem("title")
-    document.title = "\u200E"
-  }
-}
+
 
 window.addEventListener('load', function() {
-  var title = localStorage.getItem("title")
+console.log("loaded")
+var theTheme = localStorage.getItem("theme")
+//Inverted Colors theme
+if (localStorage.getItem("theme") == "InvertColorsTheme") {
 
-  if (localStorage.getItem("title") !== null) {
-    document.title = title
-  } else {
-    document.title = "NEBULA"
+console.log("done")
+let htmlL = document.documentElement;
+htmlL.style.filter = "saturate(0)";
+htmlL.style.filter = "invert(100%)";
+ }
+// Saturated theme
+ else if (localStorage.getItem("theme") == "Satur") {
+document.getElementById("theme-container").style.filter = "invert(0%)";
+document.getElementById("theme-container").style.filter = "saturate(8)";
+console.log("done")
+let htmlL = document.documentElement;
+ htmlL.style.filter = "saturate(80)";
+ htmlL.style.filter = "invert(0%)";
+ }
+// Sepia Theme
+ else if (localStorage.getItem("theme") == "Sepia") {
+document.getElementById("theme-container").style.filter = "invert(0%)";
+document.getElementById("theme-container").style.filter = "saturate(0%)";
+document.getElementById("theme-container").style.filter = "sepia(100%)";
+let htmlL = document.documentElement;
+htmlL.style.background = "radial-gradient(ellipse at bottom, rgb(94, 71, 7) 0%, rgb(112, 97, 10) 100%)";
+ }
+ // Hacker Theme
+else if (localStorage.getItem("theme") == "Hacker"){
+let htmlL = document.documentElement;
+htmlL.style.background = "radial-gradient(ellipse at bottom, rgb(0, 0, 0) 0%, rgb(13, 12, 12) 100%)";
+document.getElementById("heads").style.color = "#04ff00";
+document.getElementById("heads-1").style.color = "#04ff00";
+document.getElementById("heads-2").style.color = "#04ff00";
+document.getElementById("heads-3").style.color = "#04ff00";
+document.getElementById("heads-4").style.color = "#04ff00";
+document.getElementById("heads-5").style.color = "#04ff00";
+document.getElementById("heads-6").style.color = "#04ff00";
+document.getElementById("heads-7").style.color = "#04ff00";
+document.getElementById("heads-8").style.color = "#04ff00";
+document.getElementById("heads-9").style.color = "#04ff00";
+document.getElementById("heads-10").style.color = "#04ff00";
+}
+   else {
+    
+    document.getElementById("checker").innerHTML = "You're not using any themes.";
+    document.getElementById("theme-container").style.filter = "invert(0%)";
+    document.body.style.background = "radial-gradient(ellipse at bottom, rgb(142, 21, 255) 0%, rgb(49, 5, 59) 100%)"
   } })
-
-  // 
-
-  // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
- 
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-
-    modal.style.display = "none";
-  }
-}
-
-function discordlol() {
-  window.location.replace("https://justcool.thejangoteam.repl.co/service/hvtrs8%2F-dksaopd%2Ccmm-arp/")
-}
-
-function history() {
-  
-let url = prompt("Please insert a URL: https://example.com")
-let url2 = prompt("Please insert another URL: https://example.com")
-let url3 = prompt("Please insert one last URL: https://example.com")
-
-let yep = url.value
-let yep2 = url2.value
-let yep3 = url3.value
-
-window.open(url, "_blank");
-window.open(url2,"_blank");
-window.open(url3, "_blank");
-
-}
-
- function popup(mylink, windowname) { 
-    if (! window.focus)return true;
-    var href;
-    if (typeof(mylink) == 'string') href=mylink;
-    else href=mylink.href; 
-    window.open(href, windowname, 'width=400,height=200,scrollbars=yes'); 
-    return false; 
-  }
